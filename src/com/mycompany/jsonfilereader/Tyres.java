103eabd9ebe25f844bb59fb607b3dcaf3806066f
rolandtorma2 <rolie2300@gmail.com>
2023-11-23 12:16:24 +0100

    Issue_8
    Added javadoc at the top of the code.

--- src/com/mycompany/jsonfilereader/Tyres.java
+++ src/com/mycompany/jsonfilereader/Tyres.java
@@ -1,17 +1,38 @@
 package com.mycompany.jsonfilereader;
 
+<<<<<<< Updated upstream
 import java.util.List;
+=======
+import java.util.ArrayList;
+import java.util.List;
+import org.json.simple.JSONArray;
+>>>>>>> Stashed changes
 
 public class Tyres {
 
     int tyreCompound;
+<<<<<<< Updated upstream
     List tyrePressure;
+=======
+    List tyrePressure = new ArrayList();
+
+>>>>>>> Stashed changes
 
     public Tyres(int tyreCompound, List tyrePressure) {
         this.tyreCompound = tyreCompound;
         this.tyrePressure = tyrePressure;
     }
 
+<<<<<<< Updated upstream
+=======
+    public Tyres(int tyreCompound, JSONArray tyrePressure) {
+        this.tyreCompound = tyreCompound;
+        this.tyrePressure.addAll(tyrePressure);
+
+    }
+
+    //making getters
+>>>>>>> Stashed changes
     public int getTyreCompound() {
         return tyreCompound;
     }
