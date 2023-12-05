600085d5e9bdf0a799a12eaa4e2f9566979f502e
rolandtorma2 <rolie2300@gmail.com>
2023-12-05 18:30:54 +0100

    Cherry picked javadoc commit

--- src/com/mycompany/jsonfilereader/Setup.java
+++ src/com/mycompany/jsonfilereader/Setup.java
@@ -4,38 +4,25 @@ public class Setup {
 
     String carName;
     BasicSetup basicSetup;
-<<<<<<< Updated upstream
-
-    public Setup(String carName, BasicSetup basicSetup) {
-        this.carName = carName;
-        this.basicSetup = basicSetup;
-=======
     AdvancedSetup advancedSetup;
 
     public Setup(String carName, BasicSetup basicSetup, AdvancedSetup advancedSetup) {
         this.carName = carName;
         this.basicSetup = basicSetup;
         this.advancedSetup = advancedSetup;
->>>>>>> Stashed changes
     }
 
     public String getCarName() {
         return carName;
     }
 
-<<<<<<< Updated upstream
-=======
     //get's the data from basicSetup
->>>>>>> Stashed changes
     public BasicSetup getBasicSetup() {
         return basicSetup;
     }
 
-<<<<<<< Updated upstream
-=======
     public AdvancedSetup getAdvancedSetup() {
         return advancedSetup;
     }
 
->>>>>>> Stashed changes
 }
